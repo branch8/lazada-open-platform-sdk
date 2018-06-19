0d9059d9654d8adebae553e12538f5824027585e
Conrad <email@conradlo.com>
2018-06-20 00:33:38 +0800

    proxy LazadaAPI -> LazadaClient

--- src/__tests__/LazadaAPI.test.js
+++ src/__tests__/LazadaAPI.test.js
@@ -3,10 +3,10 @@ const { VENTURE } = require('src/LazadaClient/constants')
 
 describe('test LazadaAPI.js', () => {
   test('test create new LazadaAPI', () => {
-    console.log(LazadaAPI)
     const appKey = '123'
     const appSecret = 'abc'
     const lazadaAPI = new LazadaAPI(appKey, appSecret, VENTURE.SINGAPORE)
-    console.log(lazadaAPI)
+    expect(lazadaAPI.appKey).toEqual(appKey)
+    expect(lazadaAPI.appSecret).toEqual(appSecret)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
+// @flow
 'use strict'
 
-const LazadaClient = require('./LazadaClient')
-module.exports = LazadaClient
+import LazadaAPI from 'src/LazadaAPI'
+module.exports = (
+  appKey: string,
+  appSecret: string,
+  countryCode: string,
+  accessToken: ?string,
+) => {
+  const forwardPropertyHandler = {
+    get: (aLazadaAPI, property) => {
+      if (property in aLazadaAPI) {
+        return (aLazadaAPI: Object)[property]
+      } else if (aLazadaAPI.client !== undefined) {
+        return aLazadaAPI.client[property]
+      }
+      return undefined
+    },
+  }
+  const lazadaAPI = new LazadaAPI(appKey, appSecret, countryCode, accessToken)
+  return new Proxy(lazadaAPI, forwardPropertyHandler)
+}

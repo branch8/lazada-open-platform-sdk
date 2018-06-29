385dd7cc2debd0ef987c9aff1242e7edff706643
Conrad <email@conradlo.com>
2018-06-29 12:39:46 +0800

    [test] validate LazadaClient includes expected api actions

--- src/__tests__/LazadaClient.test.js
+++ src/__tests__/LazadaClient.test.js
@@ -2,8 +2,48 @@ const LazadaClient = require('src/LazadaClient')
 const { VENTURE } = require('src/LazadaClient/constants')
 
 describe('test LazadaClient.js', () => {
-  test('test LazadaClient possess `system` action', () => {
+  test("test LazadaClient 'system' namespace action", () => {
     const actions = ['generateAccessToken', 'refreshAccessToken']
+    for (let actionName of actions) {
+      expect(LazadaClient.hasOwnProperty(actionName)).toBe(true)
+    }
+  })
+  test("test LazadaClient 'product' namespace action", () => {
+    const actions = [
+      'getProducts',
+      'getCategoryTree',
+      'getCategoryAttributes',
+      'getBrands',
+      'createProduct',
+      'updateProduct',
+      'migrateImage',
+      'setImages',
+      'updatePriceQuantity',
+      'removeProduct',
+    ]
+    for (let actionName of actions) {
+      expect(LazadaClient.hasOwnProperty(actionName)).toBe(true)
+    }
+  })
+  test("test LazadaClient 'order' namespace action", () => {
+    const actions = [
+      'getOrders',
+      'getOrder',
+      'getOrderItems',
+      'getMultipleOrderItems',
+      'getFailureReasons',
+      'setStatusToCanceled',
+      'setStatusToReadyToShip',
+      'setStatusToPackedByMarketplace',
+      'setInvoiceNumber',
+      'getDocument',
+    ]
+    for (let actionName of actions) {
+      expect(LazadaClient.hasOwnProperty(actionName)).toBe(true)
+    }
+  })
+  test("test LazadaClient 'logistics' namespace action", () => {
+    const actions = ['getShipmentProviders']
     for (let actionName of actions) {
       expect(LazadaClient.hasOwnProperty(actionName)).toBe(true)
     }
